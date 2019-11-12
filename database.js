32f6ffb8283814758016f6d001fd5303a2f53f92
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2019-11-12 19:06:36 +0530

    huge changes

--- database.js
+++ database.js
@@ -19,22 +19,60 @@ connection.connect((err)=>{
 })
 app.use(bodyParser.json());
 app.use(cors());
-app.get('/',(req,res)=>{
-    res.send('hello');
-})
-app.post('/signUp',(req,res)=>{
-    res.send('hello');
+app.post('/signIn',(req,res)=>{
     console.log('connected');
-    console.log(req.body.password);
+    let{Email,Password}=req.body;
+    let sql="select * from user_details where Email='"+Email+"' and password='"+Password+"'";
+    connection.query(sql,(error,result)=>{
+        console.log(result);
+        if(error){
+            res.send(error);
+        }
+        else
+        {if(result)
+            return(res.json({
+                data:result[0]
+            }))
+            else
+            return(res.json(
+                'noEntry'
+            ))
+        }
+    })
+})
+
+app.post('/searchResults',(req,res)=>{console.log('connected');
+   let{source,destination,startdate}=req.body;
+   let sql="select * from buses_details where source='"+source+"'and destination='"+destination+"'";
+    connection.query(sql,(error,results)=>{
+        if(error){
+            res.send(error);
+          
+        }
+        else
+        {
+            return(res.json({
+                data:results
+            }))
+        }
+    })
+    
+})
+
+app.post('/signUp',(req,res)=>{
+    
+    console.log('connected');
+    console.log(req.body);
     let sql="insert into user_details(user_id, Name, Email, password) values('"+req.body.userid+"','"+req.body.Name+"','"+req.body.Email+"','"+req.body.password+"') ";
+    
     connection.query(sql,(error,rows,fields)=>{
         if(error){
-           throw error;
+            res.send(error);
+          
         }
-        
+    })      
+    })
 
-    })})
 
-    
 
 app.listen(1337);
